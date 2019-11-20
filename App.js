e2c2cd828dcf6e3c6fff75936b4e5cec9963a652
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-20 21:00:38 +0900

    geolocation

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import {Alert} from "react-native";
 
-export default function App() {
-  return <Loading></Loading>
+export default class extends React.Component{
+  state = {
+    isLoading: true
+  }
+  getLocation = async() => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.setState({ isLoading: false});
+    } catch(error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+    
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render(){
+    const { isLoading } = this.state;
+    return isLoading ? <Loading></Loading> : null;
+  }
 }
